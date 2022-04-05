622ba2d3151e8ba22df287784339fa01ca3fe72b
pythonallday <aisaev@live.nl>
2022-04-05 23:50:42 +0200

    Added local settings

--- core/settings.py
+++ core/settings.py
@@ -140,3 +140,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
